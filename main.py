d94242e07b5a36f18a7e14168201150a7e39f1d8
Amirhossein <ah.rasoulian@gmail.com>
2021-06-10 11:14:47 +0430

    reading the input.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
-    pass
+    regular_expression = input()
+    proteins = []
+    number_of_proteins = int(input())
+    for i in range(number_of_proteins):
+        proteins.append(input())
 
 
 if __name__ == '__main__':
